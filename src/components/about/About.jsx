682e2792ccf3c3c5a5206967cb64fffa4088f080
oleg kochetov <olego8760@gmail.com>
2023-11-25 21:08:38 +0300

    feat: add css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './about.css';
+import React from "react";
+import "./about.css";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div>About</div>;
+};
 
 export default About;
